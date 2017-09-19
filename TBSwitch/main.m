39a5e04cb89c52c65c0159961322f154081f05a6
Allyson Aberg <allysonaberg@Allysons-MacBook-Pro.local>
2017-09-19 16:17:46 -0400

    Initial Commit

---
+++ TBSwitch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TBSwitch
+//
+//  Created by Allyson Aberg on 2017-09-19.
+//  Copyright © 2017 Tunnelbear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
